3d1e2e4c9b6917257c8ceef67d5ae695ea1f9ba3
mohit <mohit1987kumar7@gmail.com>
2024-02-16 12:29:30 +0530

    display message

---
+++ src/SelenuimSession/Practise.java
@@ -0,0 +1,25 @@
+package SelenuimSession;
+
+public class Practise {
+	
+	
+	
+	int a=2 ;
+	int b=3;
+	
+	int sum=a+b;
+	public int sumadd() {
+		
+		System.out.println(sum);
+		return sum;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		SelenuimPractise add=new SelenuimPractise();
+		add.sumadd();
+
+	}
+
+}
---
+++ src/SelenuimSession/SelenuimPractise.java
@@ -0,0 +1,23 @@
+package SelenuimSession;
+
+public class SelenuimPractise {
+	
+	int a=2 ;
+	int b=3;
+	
+	int sum=a+b;
+	public int sumadd() {
+		
+		System.out.println(sum);
+		return sum;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		SelenuimPractise add=new SelenuimPractise();
+		add.sumadd();
+
+	}
+
+}
